383ce8dcd8e6586e95953920b315934402453b8b
amirpoudel <amirpoudel2058@gmail.com>
2025-02-10 05:20:31 +0545

    add some seo tags

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
+import Head from "next/head";
 import Page from "./(match)/match/page";
 
 export default function Home() {
   return (
-   <>
-    <Page/>
-   </>
+    <>
+      <Head>
+        <title>Match My Company - Find Your Compatibility</title>
+        <meta name="description" content="Discover how well you match with your company! Just a fun compatibility test for professionals." />
+        <meta name="keywords" content="company match, work compatibility, career fun, job fit test" />
+        <meta name="author" content="Your Name" />
+        <meta property="og:title" content="Match My Company - Find Your Compatibility" />
+        <meta property="og:description" content="See how well you match with your company." />
+        <meta property="og:url" content="https://match-my-company.vercel.app/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="https://c.ndtvimg.com/2024-04/9kb3o88_sad-employee_625x300_15_April_24.jpeg?downsize=773:435" />
+      </Head>
+      <Page />
+    </>
   );
 }
